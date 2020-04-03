d3dd94fb460e4ddd3ea75dbac48459796027aad6
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-04-03 20:19:49 +0200

    Trying to open frontend on load instead of default express page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("../../frontend/Stat/StatIndex.html");
 });
 
 module.exports = router;
