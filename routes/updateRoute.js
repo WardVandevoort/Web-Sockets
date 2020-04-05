a36dc3f443479874de52c9af659b51023cd396ee
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-04-05 13:30:10 +0200

    App can't find the update screen

--- routes/updateRoute.js
+++ routes/updateRoute.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const statsController = require("../controllers/api/v1/stats");
 
 /* GET users listing. */
-router.put('/updatestats', function(req, res, next) {
-  res.render('update');
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
+
+router.put('/updatestats', statsController.update);
 
 module.exports = router;
