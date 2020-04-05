64f8e3abf6862eb86facd607cdcf5e360924da1d
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-04-05 16:27:26 +0200

    Changed primus.js file

--- primus/primus.js
+++ primus/primus.js
@@ -4,6 +4,11 @@ let go = (server) => {
     let primus = new Primus(server, {/* options */});
     primus.on("connection", (spark) => {
         console.log("Received spark 🎇")
+
+        spark.on("data", (data) => {
+            console.log("data");
+        });
+
     });
 }
 
