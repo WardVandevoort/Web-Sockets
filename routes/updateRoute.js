3df3c9b8e45520ab3a30801dd91530e351b75cc2
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-04-05 13:07:03 +0200

    Still trying to get update working

--- routes/updateRoute.js
+++ routes/updateRoute.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/updatestats', function(req, res, next) {
+router.put('/updatestats', function(req, res, next) {
   res.render('update');
 });
 
