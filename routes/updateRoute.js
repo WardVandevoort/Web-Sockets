747002bb8bcf28f8a13195f867ab0bdbf48fe58e
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-04-05 13:33:02 +0200

    Trying some stuff

--- routes/updateRoute.js
+++ routes/updateRoute.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const statsController = require("../controllers/api/v1/stats");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('update');
 });
 
-router.put('/updatestats', statsController.update);
 
 module.exports = router;
